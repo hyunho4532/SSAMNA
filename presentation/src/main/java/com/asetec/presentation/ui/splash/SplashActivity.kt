00f1857e05fdb79796b398e3b07edfc217aee137
hyunho4532 <kyounguk192@gmail.com>
2024-12-31 19:55:30 +0900

    feat. splash ui working

--- presentation/src/main/java/com/asetec/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/asetec/presentation/ui/splash/SplashActivity.kt
@@ -3,13 +3,35 @@ package com.asetec.presentation.ui.splash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.asetec.presentation.animation.SplashLoader
+import com.asetec.presentation.ui.tool.CircularProgress
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplashLoader()
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box (
+                    modifier = Modifier.fillMaxWidth().height(480.dp)
+                ) {
+                    SplashLoader()
+                }
+
+                CircularProgress()
+            }
         }
     }
 }

d6f19ea9027147e5cfc9e05630cf1b9c67f2c47d
hyunho4532 <kyounguk192@gmail.com>
2025-01-12 22:22:01 +0900

    remove. unnecessary folders

--- domain2/src/main/java/com/asetec/domain2/usecase/user/model/SignInResult.kt
+++
@@ -1,7 +0,0 @@
-package com.asetec.domain2.usecase.user.model
-
-data class SignInResult(
-    val id: String,
-    val email: String?,
-    val displayName: String?
-)
--- domain2/src/main/java/com/asetec/domain2/usecase/user/repository/AuthenticationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.asetec.domain2.usecase.user.repository
-
-import com.asetec.domain2.usecase.user.model.SignInResult
-
-
-interface AuthenticationRepository {
-    suspend fun signInWithGoogle(): SignInResult
-}
